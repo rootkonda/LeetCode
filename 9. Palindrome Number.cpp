4b1f4a9cdb217fb98ecdb1ecf7d3ac81612181fb
konda89 <52867909+rootkonda@users.noreply.github.com>
2020-04-11 14:03:57 +0200

    9. Palindrome Number

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,23 @@
+/** 
+   Same as Reverse Integer problem. Just reverse the digits and compare the final value with given number.**/
+class Solution {
+public:
+    bool isPalindrome(long long x) 
+    {
+        if(x<0)
+            return false;
+        
+        long long temp=x/10;
+        long long res=x%10;
+        
+        while(temp!=0)
+        {
+            res*=10;
+            res+=(temp%10);
+            temp/=10;
+        }
+        if(res==x)
+            return true;
+        return false;
+    }
+};
