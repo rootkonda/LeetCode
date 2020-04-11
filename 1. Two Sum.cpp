ee5153964dca4b6b821e3406518b718099ebcb2d
konda89 <52867909+rootkonda@users.noreply.github.com>
2020-04-11 13:49:59 +0200

    Added Comments

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,12 +1,17 @@
+/**
+   For each element, check if it exists in the map. If it exists, return the appropriate indices. 
+   Otherwise, subtract the element from target sum and store the compliment in map **/
+
+subtract each element from target sum and look if that target sum is present 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-        // 
+        // To store the compliments
         unordered_map<int,int> unik;
         vector<int> res;
-        res.push_back(-1);
-        res.push_back(-1);
+        res.push_back(-1); // default indices when no match
+        res.push_back(-1); // default indices when no match
         for(int i=0;i<nums.size();i++)
         {
             if(unik.find(nums[i])!=unik.end())
@@ -15,7 +20,7 @@ public:
                 res[1]=i;
                 return res;
             }
-            unik[target-nums[i]]=i;
+            unik[target-nums[i]]=i; // stores the compliment
         }
         return res;
     }
