6ecb94a117621e1a18db9e1bfcb0151b021eaa08
konda89 <52867909+rootkonda@users.noreply.github.com>
2020-04-11 14:00:18 +0200

    Reverse Integer

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,24 @@
+/**
+  Simple approach. Initialize res to unit digit and truncate x by last digit. 
+  Run a loop until x becomes 0 or no more digits to reverse. Every time multiple by 10 and add the next digit. **/
+class Solution 
+{
+public:
+    int reverse(int x) 
+    {
+        long res=(x%10);
+        x/=10;
+        while(x!=0)
+        {
+            if(abs(res*10) > 2147483640)
+                return 0;
+            else
+            {
+                res=res*10;
+                res+=(x%10);
+                x/=10;
+            }  
+        }
+        return res;
+    }
+};
