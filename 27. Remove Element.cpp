a8a5e3638b5cd1cb41842267db32d179ac11e238
konda89 <52867909+rootkonda@users.noreply.github.com>
2020-04-11 15:47:48 +0200

    Remove Element

--- 27. Remove Element.cpp
+++ 27. Remove Element.cpp
@@ -1,5 +1,14 @@
 /**
+  Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+  Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+  The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+  
+  Approach:
+  
   Same approach as problem 26. Keep a pointer at 0th index and increment when element at that index doesnt match with val. **/
+  
 class Solution 
 {
 public:
