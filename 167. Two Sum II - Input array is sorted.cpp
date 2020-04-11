6eab6394d4d9b902e86dcff1aebcd341460f65b9
konda89 <52867909+rootkonda@users.noreply.github.com>
2020-04-12 00:29:04 +0200

    Two Sum II - Input array is sorted

---
+++ 167. Two Sum II - Input array is sorted.cpp
@@ -0,0 +1,7 @@
+/**
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+
+**/
