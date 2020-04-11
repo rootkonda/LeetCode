13f9a6eb05e9d3e30bfebae66807b50541fa95db
konda89 <52867909+rootkonda@users.noreply.github.com>
2020-04-11 15:45:01 +0200

    Valid Parentheses

--- 20. Valid Parentheses.cpp
+++ 20. Valid Parentheses.cpp
@@ -1,6 +1,17 @@
 /**
+  Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+  An input string is valid if:
+
+  Open brackets must be closed by the same type of brackets.
+  Open brackets must be closed in the correct order.
+  Note that an empty string is also considered valid.
+  
+  Approach:
+  
   Can be done using Stack. This is similar approach using array. Keep storing the opening brackets in an array.
   And also keep track of last open bracket index. When there is a closing bracket compare it with the last open bracket. **/
+  
 class Solution {
 public:
     bool isValid(string s) 
