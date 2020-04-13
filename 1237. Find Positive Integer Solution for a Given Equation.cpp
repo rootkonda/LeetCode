27e07be32f916a0c335fd8b2555a2c7ceeec3ba3
konda89 <52867909+rootkonda@users.noreply.github.com>
2020-04-13 17:08:25 +0200

    Find Positive Integer Solution for a Given Equation

---
+++ 1237. Find Positive Integer Solution for a Given Equation.cpp
@@ -0,0 +1,55 @@
+/**
+Given a function  f(x, y) and a value z, return all positive integer pairs x and y where f(x,y) == z.
+
+The function is constantly increasing, i.e.:
+
+f(x, y) < f(x + 1, y)
+f(x, y) < f(x, y + 1)
+The function interface is defined like this: 
+
+interface CustomFunction {
+public:
+  // Returns positive integer f(x, y) for any given positive integer x and y.
+  int f(int x, int y);
+};
+For custom testing purposes you're given an integer function_id and a target z as input, where function_id represent one function from an secret internal list, on the examples you'll know only two functions from the list.  
+
+You may return the solutions in any order.
+
+Simple. Just x and y loops from 1 to z and result of x and y should be equal to given target.
+**/
+
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ * public:
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     int f(int x, int y);
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> findSolution(CustomFunction& customfunction, int z) 
+    {
+        vector<vector<int>> ans;
+        int res;
+        for(int i=1;i<=z;i++)
+        {
+            for(int j=1;j<=z;j++)
+            {
+                res=customfunction.f(i,j);
+                if(res==z)
+                    ans.push_back({i,j});
+                else if(res>z)
+                    break;
+            }
+                
+        }
+        
+        return ans;
+    }
+};
