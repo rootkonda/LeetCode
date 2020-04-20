b65a9f6489c5eb7a4e7288b60e3034a374219984
konda89 <52867909+rootkonda@users.noreply.github.com>
2020-04-20 17:53:01 +0200

    Update 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp

--- 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -1,5 +1,9 @@
 /**
+Your are given an array of integers prices, for which the i-th element is the price of a given stock on day i; and a non-negative integer fee representing a transaction fee.
 
+You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction. You may not buy more than 1 share of a stock at a time (ie. you must sell the stock share before you buy again.)
+
+Return the maximum profit you can make.
 
 At a given day, we should decide two things. One is to either buy a stock or do nothing. Secondly, sell a stock or do nothing.
 
